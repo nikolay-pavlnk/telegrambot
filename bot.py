7c6fa1e9205abbf6ce977a42df3db865c4f49583
nikolay-pavlnk <pavlenkokolya421@gmail.com>
2019-04-13 16:31:58 +0300

    telegram

--- bot.py
+++ bot.py
@@ -12,15 +12,13 @@ server = Flask(__name__)
 
 
 @server.route("/", methods=['POST'])
-    def getMessage():
-
-
-   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+def getMessage():
+	bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
         return "!", 200
 
 @server.route("/")
 def webhook():
-    bot.remove_webhook()
+	bot.remove_webhook()
     bot.set_webhook(url="https://telegrambotfortest.herokuapp.com/")
     return "!", 200
 
