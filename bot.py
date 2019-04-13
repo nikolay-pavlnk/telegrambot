62f7808f4348510d75fb981ae9e8282246dce082
nikolay-pavlnk <pavlenkokolya421@gmail.com>
2019-04-13 16:24:28 +0300

    telegram

--- bot.py
+++ bot.py
@@ -1,21 +1,28 @@
-import telebot
 import os
+import telebot
 from flask import Flask, request
-import logging
 
 bot = telebot.TeleBot('824073635:AAFokg_1q7Wyj8VvkBgXm7SSfJ8kfXr7SJs')
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.INFO)
 
 server = Flask(__name__)
-@server.route("/bot", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
+
+#hadlers
+#and in the end
+
+
+@server.route("/", methods=['POST'])
+    def getMessage():
+
+
+   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+
 @server.route("/")
 def webhook():
-  	bot.remove_webhook()
-    bot.set_webhook(url="https://telegrambotfortest.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
-    return "?", 200
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+    bot.remove_webhook()
+    bot.set_webhook(url="https://telegrambotfortest.herokuapp.com/")
+    return "!", 200
+
+server.run(host="0.0.0.0", port=5000)
+server = Flask(__name__)
