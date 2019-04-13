89c2cabf08e5e68af0f631e4ad43cc4e0ca923c0
nikolay-pavlnk <pavlenkokolya421@gmail.com>
2019-04-13 17:09:08 +0300

    telegram

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ from telebot import types
 import telebot
 from config import *
 import os
+
+TOKEN = '824073635:AAFokg_1q7Wyj8VvkBgXm7SSfJ8kfXr7SJs'
+APP_NAME = 'telegrambotfortest'
  
 server = flask.Flask(__name__)
 bot = telebot.TeleBot(TOKEN)
--- config.py
+++
@@ -1,2 +0,0 @@
-TOKEN = '824073635:AAFokg_1q7Wyj8VvkBgXm7SSfJ8kfXr7SJs'
-APP_NAME = 'telegrambotfortest'
