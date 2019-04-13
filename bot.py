3da014ca406b6c2710459ad27fe921c036e18c1e
nikolay-pavlnk <pavlenkokolya421@gmail.com>
2019-04-13 16:32:58 +0300

    telegram

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ server = Flask(__name__)
 @server.route("/", methods=['POST'])
 def getMessage():
 	bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
+    return "!", 200
 
 @server.route("/")
 def webhook():
